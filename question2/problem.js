55c5ff3608ce56967adeac34575a78127e8d941a
John Lange <lange.john.m@gmail.com>
2022-03-25 12:33:13 -0400

    Finish up the fastfib stub from question 2

--- question2/problem.js
+++ question2/problem.js
@@ -18,14 +18,32 @@ function fib(number)  {
 
     return fm1 + fm2;
 }
+// this will get the right answer, but takes a bit. Turns out, it still finished before I could write the below, so it was fast enough
 
-function fastfib(number) { 
-    let i = 0;
-    while(i <= number)
+
+
+const fastfib_lib = {};
+fastfib_lib[0] = 1;
+fastfib_lib[1] = 1;
+
+function fastfib(number) { // a way of doing a faster fib function
+    if(!fastfib_lib[number])
     {
+        let fm1 = fastfib(number - 1);
+        if(fm1 % 2 == 0)
+        {
+            fm1 = fm1/2;
+        }
+    
+        let fm2 = fastfib(number - 2);
+        if(fm2 % 7 == 0)
+        {
+            fm2 += 1;
+        }
         
+        fastfib_lib[number] = fm1 + fm2;
     }
-
+    return fastfib_lib[number];
 }
 
-console.log(fib(50));
+console.log(fastfib(50));
